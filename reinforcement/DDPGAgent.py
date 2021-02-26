1fcc5e5cdbd86ce45405aa01a6235ca905f95bb7
AlexanderLutsenko <lex.lutsenko@gmail.com>
2021-02-26 12:28:40 +0300

    _

---
+++ reinforcement/DDPGAgent.py
@@ -0,0 +1,208 @@
+import random
+
+import numpy as np
+from collections import namedtuple
+
+import torch
+import torch.nn.functional as F
+
+
+Sample = namedtuple('Sample',
+                    ('state', 'action', 'reward', 'next_state', 'non_terminal'))
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        # Saves a training sample
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Sample(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, min(len(self.memory), batch_size))
+
+    def __len__(self):
+        return len(self.memory)
+
+
+def UONoise():
+    theta = 0.15
+    sigma = 0.2
+    state = 0
+    while True:
+        yield state
+        state += -theta*state+sigma*np.random.randn()
+
+
+class DDPGAgent(object):
+
+    def __init__(self,
+                 actor_builder, critic_builder,
+                 actor_optimizer_builder, critic_optimizer_builder,
+                 device,
+
+                 num_actions,
+
+                 replay_memory_size,
+                 replay_memory_warmup_size,
+                 batch_size,
+
+                 gamma,
+                 eps_start,
+                 eps_end,
+                 eps_decay,
+
+                 target_update_frequency,
+                 repeat_update=1
+                 ):
+
+        self.device = device
+        self.num_actions = num_actions
+
+        self.batch_size = batch_size
+        self.gamma = gamma
+        self.eps_start = eps_start
+        self.eps_end = eps_end
+        self.eps_decay = eps_decay
+        self.target_update_frequency = target_update_frequency
+        self.repeat_update = repeat_update
+
+        self.actor_net = actor_builder().to(device)
+        self.critic_net = critic_builder().to(device)
+
+        self.actor_target_net = actor_builder().to(device)
+        self.actor_target_net.load_state_dict(self.actor_net.state_dict())
+        self.actor_target_net.eval()
+
+        self.critic_target_net = critic_builder().to(device)
+        self.critic_target_net.load_state_dict(self.critic_net.state_dict())
+        self.critic_target_net.eval()
+
+        self.actor_optimizer = actor_optimizer_builder(self.actor_net.parameters())
+        self.critic_optimizer = critic_optimizer_builder(self.critic_net.parameters())
+
+        # Here we'll be maintaining our "dataset", i.e. agent's past experience
+        self.memory = ReplayMemory(replay_memory_size)
+        self.replay_memory_warmup_size = replay_memory_warmup_size
+
+        self.steps_done = 0
+        self.eps_threshold = self.eps_start
+
+        self.is_training = True
+
+        self.update_tau = 0.001
+        self.noise = UONoise()
+        self.max_explore_eps = 500*200
+
+    def set_training(self, is_training):
+        self.is_training = is_training
+
+    def save_weights(self, path):
+        torch.save({
+           'actor_state_dict': self.actor_net.state_dict(),
+           'critic_state_dict': self.critic_net.state_dict()
+        }, path)
+
+    def load_weights(self, path):
+        checkpoint = torch.load(path)
+        self.actor_net.load_state_dict(checkpoint['actor_state_dict'])
+        self.critic_net.load_state_dict(checkpoint['critic_state_dict'])
+
+        self.actor_target_net.load_state_dict(self.actor_net.state_dict())
+        self.critic_target_net.load_state_dict(self.critic_net.state_dict())
+
+    def update_target_net(self, target, source, update_tau):
+        for target_param, param in zip(target.parameters(), source.parameters()):
+            target_param.data.copy_(
+                target_param.data * (1.0 - update_tau) + param.data * update_tau
+            )
+
+    def act(self, state):
+        self.steps_done += 1
+        with torch.no_grad():
+            th_state = torch.from_numpy(state).unsqueeze(0).type(torch.get_default_dtype()).to(self.device)
+            self.actor_net.train(False)
+            action = self.actor_net(th_state).detach().cpu().numpy().squeeze(axis=0)
+            self.actor_net.train(True)
+
+            # if self.is_training:
+            #     self.eps_threshold = self.eps_start - (self.eps_start - self.eps_end) * min(1, self.steps_done / self.eps_decay)
+            #     # noise = np.random.normal(scale=self.eps_threshold, size=action.shape)
+            #     noise = np.random.normal(scale=self.eps_threshold, size=action.shape)
+            #     action = action + noise
+
+            if self.is_training and self.steps_done < self.max_explore_eps:
+                p = self.steps_done / self.max_explore_eps
+                action = action * p + (1 - p) * next(self.noise) * 2
+
+            return action
+
+    def observe_and_learn(self, state, action, reward, next_state, is_terminal):
+        if not self.is_training:
+            return
+
+        self.memory.push(
+            torch.tensor(state),
+            torch.tensor(action, dtype=torch.long),
+            torch.tensor(reward, dtype=torch.get_default_dtype()),
+            torch.tensor(next_state),
+            torch.tensor(0 if is_terminal else 1, dtype=torch.get_default_dtype())
+        )
+
+        self.learn()
+
+    def learn(self):
+
+        if len(self.memory) < max(self.batch_size, self.replay_memory_warmup_size):
+            return
+
+        for _ in range(self.repeat_update):
+            samples = self.memory.sample(self.batch_size)
+            # Transpose the batch (see http://stackoverflow.com/a/19343/3343043 for detailed explanation).
+            batch = Sample(*zip(*samples))
+
+            # Compute a mask of non-final states and concatenate the batch elements
+            state_batch = torch.stack(batch.state).type(torch.get_default_dtype()).to(self.device)
+            action_batch = torch.stack(batch.action).to(self.device)
+            reward_batch = torch.stack(batch.reward).to(self.device)
+            next_state_batch = torch.stack(batch.next_state).type(torch.get_default_dtype()).to(self.device)
+            non_terminal_mask = torch.stack(batch.non_terminal).to(self.device)
+
+            # Compute Q(s_t, mu(s_t))
+            Q_values = self.critic_net(state_batch, action_batch).squeeze(dim=1)
+
+            # Compute Q'(s_{t+1}, mu'(s_{t+1}))
+            with torch.no_grad():
+                next_Q_values = self.critic_target_net(next_state_batch, self.actor_target_net(next_state_batch)).squeeze(dim=1)
+                expected_Q_values = (next_Q_values * non_terminal_mask * self.gamma) + reward_batch
+
+            # print(Q_values.shape, expected_Q_values.shape)
+
+            critic_loss = F.mse_loss(Q_values, expected_Q_values)
+
+            # Optimize the critic
+            self.critic_optimizer.zero_grad()
+            critic_loss.backward()
+            self.critic_optimizer.step()
+
+            actor_loss = -self.critic_net(state_batch, self.actor_net(state_batch)).mean()
+
+            # Optimize the actor
+            self.actor_optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor_optimizer.step()
+
+        self.update_target_net(self.actor_target_net, self.actor_net, self.update_tau)
+        self.update_target_net(self.critic_target_net, self.critic_net, self.update_tau)
+
+        # if self.steps_done % self.target_update_frequency == 0:
+        #     # It's time to update target_net
+        #     self.actor_target_net.load_state_dict(self.actor_net.state_dict())
+        #     self.critic_target_net.load_state_dict(self.critic_net.state_dict())
---
+++ reinforcement/pendulum/train_ddpg.py
@@ -0,0 +1,223 @@
+import argparse
+import os
+
+import gym
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import random
+
+from reinforcement.DDPGAgent import DDPGAgent
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--train', type=lambda x: bool(int(x)),
+                        default=True,
+                        # default=False
+                        )
+    parser.add_argument('--visualize', type=lambda x: bool(int(x)),
+                        default=True
+                        # default=False
+                        )
+    parser.add_argument('--load_from_checkpoint', type=lambda x: bool(int(x)),
+                        # default=True
+                        default=False
+                        )
+    parser.add_argument('--checkpoint_dir', type=str,
+                        default='./checkpoints'
+                        )
+    parser.add_argument('--checkpoint_frequency', type=int,
+                        default=10
+                        )
+
+    # Training parameters
+
+    # Number of games to play
+    parser.add_argument('--num_episodes', type=int,
+                        default=1000
+                        )
+    # Max dataset size
+    parser.add_argument('--replay_memory_size', type=int,
+                        default=100000
+                        )
+    # Agent won't start training until memory is big enough
+    parser.add_argument('--replay_memory_warmup_size', type=int,
+                        default=256*3
+                        )
+
+    parser.add_argument('--batch_size', type=int,
+                        default=256
+                        )
+    parser.add_argument('--actor_learning_rate', type=float,
+                        default=0.0001
+                        )
+    parser.add_argument('--critic_learning_rate', type=float,
+                        default=0.001
+                        )
+    parser.add_argument('--momentum', type=float,
+                        default=0.8
+                        )
+
+    # Total reward discount
+    parser.add_argument('--gamma', type=float,
+                        default=0.99
+                        )
+
+    # epsilon-greedy parameters:
+    # chance to perform random action decreases from eps_start to eps_end over the course of eps_decay steps
+    parser.add_argument('--eps_start', type=float,
+                        default=1
+                        )
+    parser.add_argument('--eps_end', type=float,
+                        default=0.3
+                        )
+    parser.add_argument('--eps_decay', type=int,
+                        default=50000
+                        )
+
+    # target_net update frequency
+    parser.add_argument('--target_update_frequency', type=int,
+                        default=100
+                        )
+    # Number of training steps per observation step
+    parser.add_argument('--repeat_update', type=int,
+                        default=1
+                        )
+
+    # random seed
+    parser.add_argument('--seed', type=int,
+                        default=42
+                        )
+
+    args = parser.parse_args()
+    return args
+
+
+class Actor(nn.Module):
+    def __init__(self, state_size, num_actions):
+        super().__init__()
+        num_hidden = 64
+        self.net = nn.Sequential(
+            nn.Linear(state_size, num_hidden),
+            nn.BatchNorm1d(num_hidden),
+            nn.ELU(),
+            nn.Linear(num_hidden, num_hidden),
+            nn.BatchNorm1d(num_hidden),
+            nn.ELU(),
+            nn.Linear(num_hidden, num_actions),
+            nn.Sigmoid()
+        )
+        self.min_val = float(environment.action_space.low)
+        self.max_val = float(environment.action_space.high)
+
+    def forward(self, states):
+        actions_unscaled = self.net(states)
+        return self.min_val + actions_unscaled * (self.max_val - self.min_val)
+
+
+class Critic(nn.Module):
+    def __init__(self, state_size, num_actions):
+        super().__init__()
+        num_hidden = 64
+        self.net = nn.Sequential(
+            nn.Linear(state_size + num_actions, num_hidden),
+            nn.BatchNorm1d(num_hidden),
+            nn.ELU(),
+            nn.Linear(num_hidden, num_hidden),
+            nn.BatchNorm1d(num_hidden),
+            nn.ELU(),
+            nn.Linear(num_hidden, 1)
+        )
+
+    def forward(self, states, actions):
+        return self.net(torch.cat([states, actions], dim=1))
+
+
+def create_agent(args, state_size, num_actions):
+
+    def actor_builder():
+        return Actor(state_size, num_actions)
+
+    def critic_builder():
+        return Critic(state_size, num_actions)
+
+    # optimizer_builder = lambda parameters: optim.SGD(parameters, lr=args.learning_rate, momentum=args.momentum)
+    actor_optimizer_builder = lambda parameters: optim.Adam(parameters, lr=args.actor_learning_rate)
+    critic_optimizer_builder = lambda parameters: optim.Adam(parameters, lr=args.critic_learning_rate)
+
+    agent = DDPGAgent(actor_builder, critic_builder, actor_optimizer_builder, critic_optimizer_builder,
+                     device, num_actions,
+                     gamma=args.gamma,
+
+                     replay_memory_size=args.replay_memory_size,
+                     replay_memory_warmup_size=args.replay_memory_warmup_size,
+                     batch_size=args.batch_size,
+
+                     eps_start=args.eps_start,
+                     eps_end=args.eps_end,
+                     eps_decay=args.eps_decay,
+
+                     target_update_frequency=args.target_update_frequency,
+
+                     repeat_update=args.repeat_update
+                     )
+    return agent
+
+
+if __name__ == '__main__':
+    args = get_args()
+
+    # Device configuration
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    torch.set_default_tensor_type('torch.FloatTensor')
+
+    env_name = 'Pendulum-v0'
+    environment = gym.make(env_name)
+
+    # Set fixed random seeds so your experiments are reproducible
+    torch.manual_seed(args.seed)
+    random.seed(args.seed)
+    environment.seed(args.seed)
+
+    print('Observation space: {}'.format(environment.observation_space))
+    print('Action space: {}'.format(environment.action_space))
+
+    state_size = environment.observation_space.shape[0]
+    num_actions = environment.action_space.shape[0]
+    agent = create_agent(args, state_size, num_actions)
+
+    if args.load_from_checkpoint:
+        agent.load_weights(os.path.join(args.checkpoint_dir, env_name))
+
+    agent.set_training(args.train)
+
+    total_steps = 0
+    for episode in range(args.num_episodes):
+        last_state = environment.reset()
+        total_reward = 0
+        while True:
+            if args.visualize:
+                environment.render()
+
+            actions = agent.act(last_state)
+            actions = np.clip(actions, -2, 2)
+
+            next_state, reward, is_terminal, info = environment.step(actions)
+
+            agent.observe_and_learn(last_state, actions, reward, next_state, is_terminal)
+
+            total_steps += 1
+            total_reward += reward
+            last_state = next_state
+
+            if is_terminal:
+                print('Episode {}, steps: {}, reward: {} '.format(episode, total_steps, total_reward))
+
+                if args.train and episode % args.checkpoint_frequency == 0:
+                    os.makedirs(args.checkpoint_dir, exist_ok=True)
+                    agent.save_weights(os.path.join(args.checkpoint_dir, env_name))
+                    print('Checkpoint created')
+                break
